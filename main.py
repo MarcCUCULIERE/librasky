f415c076a388eaca7a755d012fefb39d0d28b90f
Marc CUCULIERE <marc.cuculiere@younited-credit.fr>
2024-09-01 15:51:06 +0200

    chore: Update main.py and youtils.py

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
-import pyodbc
+import logging
 import youtils
-import traceback
-import json
-import time
-from datetime import datetime, date
 from flask import Flask, request, jsonify
 from flask_restx import Api, Resource, fields
-  
+
+# Configure logging
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
 app = Flask(__name__)
-api = Api(app, version='1.0', title='Librasky', description='Une API simple pour gerer les collections.')
+api = Api(app, version='1.0', title='Librasky', description='Une API simple pour gérer les collections.')
 ns = api.namespace('/', description='Opérations principales')
 
-data_model = api.model('Create',{
+data_model = api.model('Create', {
     'Quantite': fields.Integer(required=True, description='Quantité de bouteille détenue(s)'),
     'Nom': fields.String(required=True, description='Nom de l\'Edition'),
     'Distillerie': fields.String(required=True, description='Distillerie'),
@@ -22,120 +22,61 @@ data_model = api.model('Create',{
     'Prix': fields.Float(required=True, description='Prix')
 })
 
-def fetch_data():
-    conn = None
-    try:
-        conn = youtils.connect_to_sql_server()
-        if conn is None:
-            raise ConnectionError("Failed to connect to the database.")
-        cursor = conn.cursor()
-        cursor.execute("SELECT Quantite, Nom, Distillerie, Année, Age, Degrés, Date_achat, Prix FROM Collection")
-        columns = [column[0] for column in cursor.description]
-        data = [dict(zip(columns, row)) for row in cursor.fetchall()]
-        return data
-    except pyodbc.Error as err:
-        app.logger.error(f"Database error: {err}")
-        traceback.print_exc()  # Log the full stack trace
-        return {"error": "Database operation failed"}, 500
-    except ConnectionError as e:
-        app.logger.error(f"Connection error: {e}")
-        traceback.print_exc()  # Log the full stack trace
-        return {"error": "Failed to connect to the database"}, 500
-    finally:
-        if conn:
-            conn.close()
-
-# Example of a simple health check endpoint
 @api.route('/health/ping')
 class HealthCheck(Resource):
     def get(self):
-        try:
-            conn = youtils.connect_to_sql_server()
-            if conn is None:
-                raise ConnectionError("Database connection failed")
+        conn = youtils.connect_to_sql_server()
+        if conn:
+            conn.close()
             return "pong", 200
-        except ConnectionError as e:
-            app.logger.error(f"Health check failed: {e}")
-            return {"status": "unhealthy"}, 503
+        return {"status": "unhealthy"}, 503
 
 @api.route('/create')
 class InsertData(Resource):
     @api.expect(data_model)
     def post(self):
         data = request.json
-        quantite = data.get("Quantite")
-        nom = data.get("Nom")
-        distillerie = data.get("Distillerie")
-        annee = data.get("Année")
-        age = data.get("Age")
-        degres = data.get("Degrés")
-        date_achat = data.get("Date_achat")
-        prix = data.get("Prix")
-        try:
-            conn = youtils.connect_to_sql_server()
-            cursor = conn.cursor()
-            cursor.execute("INSERT INTO Collection (Quantite, Nom, Distillerie, Année, Age, Degrés, Date_achat, Prix) VALUES (?, ?, ?, ?, ?, ?, ?, ?)", 
-                       (quantite, nom, distillerie, annee, age, degres, date_achat, prix))
-            conn.commit()
-            print(f"Données insérées avec succès : {quantite}, {nom}, {distillerie}, {annee}, {age}, {degres}, {date_achat}, {prix}")
-        except pyodbc.Error as e:
-            print("Erreur lors de l'insertion des données:", str(e))
-        finally:
-            if conn:
-                conn.close()
-        return jsonify({"message": "Data inserted successfully", "data": data})
+        query = "INSERT INTO Collection (Quantite, Nom, Distillerie, Année, Age, Degrés, Date_achat, Prix) VALUES (?, ?, ?, ?, ?, ?, ?, ?)"
+        params = (data['Quantite'], data['Nom'], data['Distillerie'], data['Année'], data['Age'], data['Degrés'], data['Date_achat'], data['Prix'])
+        conn, cursor = youtils.execute_query(query, params)
+        if cursor:
+            conn.close()
+            return jsonify({"message": "Data inserted successfully", "data": data})
+        return {"error": "Failed to insert data"}, 500
 
 @api.route('/list')
 class ListData(Resource):
     def get(self):
-        data = fetch_data()
-        return jsonify(data)
-    
+        data = youtils.fetch_data()
+        if data:
+            return jsonify(data)
+        return {"error": "Failed to fetch data"}, 500
+
 @api.route('/update')
 class UpdateData(Resource):
     @api.expect(data_model)
     def put(self):
         data = request.json
-        quantite = data.get("Quantite")
-        nom = data.get("Nom")
-        distillerie = data.get("Distillerie")
-        annee = data.get("Année")
-        age = data.get("Age")
-        degres = data.get("Degrés")
-        date_achat = data.get("Date_achat")
-        prix = data.get("Prix")
-        try:
-            conn = youtils.connect_to_sql_server()
-            cursor = conn.cursor()
-            cursor.execute("UPDATE Collection SET Quantite=?, Nom=?, Distillerie=?, Année=?, Age=?, Degrés=?, Date_achat=?, Prix=? WHERE Nom=?", 
-                       (quantite, nom, distillerie, annee, age, degres, date_achat, prix, nom))
-            conn.commit()
-            print(f"Données mises à jour avec succès : {quantite}, {nom}, {distillerie}, {annee}, {age}, {degres}, {date_achat}, {prix}")
-        except pyodbc.Error as e:
-            print("Erreur lors de la mise à jour des données:", str(e))
-        finally:
-            if conn:
-                conn.close()
-        return jsonify({"message": "Data updated successfully", "data": data})
+        query = "UPDATE Collection SET Quantite=?, Nom=?, Distillerie=?, Année=?, Age=?, Degrés=?, Date_achat=?, Prix=? WHERE Nom=?"
+        params = (data['Quantite'], data['Nom'], data['Distillerie'], data['Année'], data['Age'], data['Degrés'], data['Date_achat'], data['Prix'], data['Nom'])
+        conn, cursor = youtils.execute_query(query, params)
+        if cursor:
+            conn.close()
+            return jsonify({"message": "Data updated successfully", "data": data})
+        return {"error": "Failed to update data"}, 500
 
 @api.route('/delete')
 class DeleteData(Resource):
     @api.expect(data_model)
     def delete(self):
         data = request.json
-        nom = data.get("Nom", "Distillerie", "Année", "Age", "Degrés", "Date_achat", "Prix")
-        try:
-            conn = youtils.connect_to_sql_server()
-            cursor = conn.cursor()
-            cursor.execute("DELETE FROM Collection WHERE Nom=?, Distillerie=?, Quantite=?, ", (quantite, nom,))
-            conn.commit()
-            print(f"Données supprimées avec succès : {nom}")
-        except pyodbc.Error as e:
-            print("Erreur lors de la suppression des données:", str(e))
-        finally:
-            if conn:
-                conn.close()
-        return jsonify({"message": "Data deleted successfully", "data": data})
+        query = "DELETE FROM Collection WHERE Nom=?"
+        params = (data['Nom'],)
+        conn, cursor = youtils.execute_query(query, params)
+        if cursor:
+            conn.close()
+            return jsonify({"message": "Data deleted successfully", "data": data})
+        return {"error": "Failed to delete data"}, 500
 
 @api.route('/search')
 class SearchData(Resource):
@@ -147,4 +88,4 @@ class SearchData(Resource):
         return jsonify(result)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000,debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
--- youtils.py
+++ youtils.py
@@ -1,62 +1,114 @@
 import os
 import pyodbc
+import logging
+from azure.identity import DefaultAzureCredential
+from azure.keyvault.secrets import SecretClient
+
+# Configure logging
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+# Azure Key Vault configuration
+key_vault_name = os.environ.get('KEY_VAULT_NAME')
+key_vault_uri = f"https://{key_vault_name}.vault.azure.net"
+
+# Initialize the Azure Key Vault client
+credential = DefaultAzureCredential()
+client = SecretClient(vault_url=key_vault_uri, credential=credential)
+
+def get_secret(secret_name):
+    """Retrieve a secret from Azure Key Vault."""
+    try:
+        secret = client.get_secret(secret_name)
+        logger.debug(f"Secret {secret_name} retrieved successfully")
+        return secret.value
+    except Exception as e:
+        logger.error(f"Error retrieving secret {secret_name}: {e}")
+        return None
+
+def get_connection_string():
+    """Generate the connection string for SQL Server."""
+    server = 'sql-sb-marc-weu-01.database.windows.net'
+    database = 'LIBRASKY'
+    driver = '{ODBC Driver 17 for SQL Server}'
+    username = get_secret('DB-USERNAME')
+    password = get_secret('DB-PASSWORD')
+    logger.debug(f"Username: {username}")
+    logger.debug(f"Password: {password}")
+    logger.debug(f"Server: {server}")
+    logger.debug(f"Database: {database}")
+    return f'DRIVER={driver};SERVER={server};DATABASE={database};Uid={username};Pwd={password};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30'
 
 def connect_to_sql_server():
     """Function to connect to SQL Server database using username and password."""
-    server = 'tcp:sql-sb-marc-weu.database.windows.net:1443'
-    database = 'LIBRASKY'
-    driver = '{ODBC Driver 17 for SQL Server}'
-    username = os.environ.get('DB_USERNAME')
-    password = os.environ.get('DB_PASSWORD')
-    connection_string = f'DRIVER={driver};SERVER={server};DATABASE={database};Uid={username};Pwd={password};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30'
-
+    connection_string = get_connection_string()
     try:
         conn = pyodbc.connect(connection_string)
+        logger.info("Connected to SQL Server")
         return conn
     except pyodbc.Error as e:
-        print(f"Error connecting to SQL Server: {e}")
+        logger.error(f"Error connecting to SQL Server: {e}")
         return None
 
-def create_database_and_schema(server, database):
+def execute_query(query, params=None):
+    """Execute a SQL query with optional parameters."""
+    logger.debug(f"Executing query: {query} with params: {params}")
+    conn = connect_to_sql_server()
+    if conn is None:
+        logger.error("Connection to SQL Server failed")
+        return None, None
     try:
-        # Connexion au serveur avec nom d'utilisateur et mot de passe
-        conn = connect_to_sql_server()
-        if conn is None:
-            return
         cursor = conn.cursor()
-        cursor.execute(f"IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = '{database}') CREATE DATABASE [{database}]")
+        cursor.execute(query, params or ())
         conn.commit()
-        print(f"Base de données '{database}' créée ou déjà existante.")
+        logger.info("Query executed successfully")
+        return conn, cursor
     except pyodbc.Error as e:
-        print("Erreur lors de la création de la base de données:", str(e))
-        return
-    
-    try:
-        # Connexion à la nouvelle base de données et création de la table
-        conn = connect_to_sql_server()
-        if conn is None:
-            return
-        cursor = conn.cursor()
-        cursor.execute('''
-            IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'Collection')
-            CREATE TABLE Collection (
-                id INT PRIMARY KEY IDENTITY(1,1),
-                Quantite INT,
-                Nom NVARCHAR(255),
-                Distillerie NVARCHAR(255),
-                Année INT,
-                Age INT,
-                Degrés FLOAT,
-                Date_achat DATE,
-                Prix FLOAT
-            )
-        ''')
-        conn.commit()
-        print("Table 'Collection' créée ou déjà existante.")
-    except pyodbc.Error as e:
-        print("Erreur lors de la création de la table 'Collection':", str(e))
+        logger.error(f"Error executing query: {e}")
+        if conn:
+            conn.close()
+        return None, None
+
+def fetch_data():
+    """Fetch data from the database."""
+    query = "SELECT Quantite, Nom, Distillerie, Année, Age, Degrés, Date_achat, Prix FROM Collection"
+    conn = connect_to_sql_server()
+    if conn:
+        try:
+            cursor = conn.cursor()
+            cursor.execute(query)
+            data = cursor.fetchall()
+            columns = [column[0] for column in cursor.description]
+            results = [dict(zip(columns, row)) for row in data]
+            conn.close()
+            return results
+        except pyodbc.Error as e:
+            logger.error(f"Error executing query: {e}")
+            conn.close()
+            return None
+    else:
+        return None
+
+def create_database_and_schema():
+    database_creation_query = "IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = 'LIBRASKY') CREATE DATABASE [LIBRASKY]"
+    table_creation_query = '''
+        IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'Collection')
+        CREATE TABLE Collection (
+            id INT PRIMARY KEY IDENTITY(1,1),
+            Quantite INT,
+            Nom NVARCHAR(255),
+            Distillerie NVARCHAR(255),
+            Année INT,
+            Age INT,
+            Degrés FLOAT,
+            Date_achat DATE,
+            Prix FLOAT
+        )
+    '''
+    if execute_query(database_creation_query):
+        logger.info("Database 'LIBRASKY' created or already exists.")
+    if execute_query(table_creation_query):
+        logger.info("Table 'Collection' created or already exists.")
 
 if __name__ == "__main__":
-    server = 'tcp:sql-sb-marc-weu.database.windows.net'
-    database = 'LIBRASKY'
-    create_database_and_schema(server, database)
+    create_database_and_schema()
